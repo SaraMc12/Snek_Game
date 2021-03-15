fdb6d4ed39ff51737da7aa3a70ace2d6f869b645
Sara McMullin <mcsara1221@gmail.com>
2021-03-15 18:37:13 -0400

    removed SnekFood.js

--- snake.js
+++ snake.js
@@ -1,24 +1,19 @@
-import { getSnakePosition } from "./movement";
+import { getInputDirection } from "./movement.js";
 
-export const snakeSpeed = 2;
-
-const snakeBody = [
-  { x: 10, y: 11 },
-  { x: 11, y: 11 },
-  { x: 12, y: 11 },
-  { x: 13, y: 11 },
-];
+export const SNAKE_SPEED = 5;
+const snakeBody = [{ x: 11, y: 11 }];
 let newSegments = 0;
 
-// update snake segments or child elements to follow the movements of the parent element or the head of the snake
 export function update() {
-  const snakePosition = getSnakePosition();
+  addSegments();
+
+  const inputDirection = getInputDirection();
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = { ...snakeBody[i] };
   }
-  //   update the movement of snakes head
-  snakeBody[0].x += snakePosition.x;
-  snakeBody[0].y += snakePosition.y;
+
+  snakeBody[0].x += inputDirection.x;
+  snakeBody[0].y += inputDirection.y;
 }
 
 export function draw(gameBoard) {
@@ -29,4 +24,35 @@ export function draw(gameBoard) {
     snakeElement.classList.add("snake");
     gameBoard.appendChild(snakeElement);
   });
+}
+
+export function expandSnake(amount) {
+  newSegments += amount;
+}
+
+export function onSnake(position, { ignoreHead = false } = {}) {
+  return snakeBody.some((segment, index) => {
+    if (ignoreHead && index === 0) return false;
+    return equalPositions(segment, position);
+  });
+}
+
+export function getSnakeHead() {
+  return snakeBody[0];
+}
+
+export function snakeIntersection() {
+  return onSnake(snakeBody[0], { ignoreHead: true });
+}
+
+function equalPositions(pos1, pos2) {
+  return pos1.x === pos2.x && pos1.y === pos2.y;
+}
+
+function addSegments() {
+  for (let i = 0; i < newSegments; i++) {
+    snakeBody.push({ ...snakeBody[snakeBody.length - 1] });
+  }
+
+  newSegments = 0;
 }
